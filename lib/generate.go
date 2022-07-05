d09c0a805ad388489f05f355032d1c5227822d49
DJ Schleen <djschleen@gmail.com>
2022-07-05 10:35:59 -0600

    More test coverage and debug logs

--- lib/generate.go
+++ lib/generate.go
@@ -1,72 +1,69 @@
 package lib
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"path"
 	"strings"
 
-	"github.com/gookit/color"
+	"github.com/devops-kung-fu/common/file"
 	"github.com/spf13/afero"
 	"gopkg.in/yaml.v2"
 )
 
-// Generator - generates the dependabot.yml in the specified repo path.
-func Generator(repoPath string, verbose bool, schedule Schedule) {
-	fs := afero.NewOsFs()
-	bundler := platform(fs, `Gemfile|Gemfile\.lock`, repoPath, "bundler", schedule)
-	cargo := platform(fs, `Cargo\.toml`, repoPath, "cargo", schedule)
-	composer := platform(fs, `composer\.json`, repoPath, "composer", schedule)
-	docker := platform(fs, `Dockerfile(.*)|docker\-compose\.yml`, repoPath, "docker", schedule)
-	elm := platform(fs, `elm\-package\.json`, repoPath, "elm", schedule)
-	gitsubmodules := platform(fs, `\.gitmodules`, repoPath, "gitsubmodule", schedule)
-	github := platform(fs, `(.*)\.(yaml|yml)`, repoPath, "github-actions", schedule)
+// Generate generates the dependabot.yml in the specified repo path.
+func Generate(afs *afero.Afero, repoPath string, schedule Schedule) (config Configuration, err error) {
+	log.Println("Starting file generation")
+
+	bundler := platform(afs, `Gemfile|Gemfile\.lock`, repoPath, "bundler", schedule)
+	cargo := platform(afs, `Cargo\.toml`, repoPath, "cargo", schedule)
+	composer := platform(afs, `composer\.json`, repoPath, "composer", schedule)
+	docker := platform(afs, `Dockerfile(.*)|docker\-compose\.yml`, repoPath, "docker", schedule)
+	elm := platform(afs, `elm\-package\.json`, repoPath, "elm", schedule)
+	gitsubmodules := platform(afs, `\.gitmodules`, repoPath, "gitsubmodule", schedule)
+	github := platform(afs, `(.*)\.(yaml|yml)`, repoPath, "github-actions", schedule)
 	var githubActual []Update
 	for _, githubPath := range github {
 		if strings.Contains(githubPath.Directory, ".github/workflows") {
 			githubActual = append(githubActual, githubPath)
 		}
 	}
-	gomod := platform(fs, `go\.mod`, repoPath, "gomod", schedule)
-	gradle := platform(fs, `build\.gradle|build\.gradle\.kts`, repoPath, "gradle", schedule)
-	hexmix := platform(fs, `mix\.exs|mix\.lock`, repoPath, "mix", schedule)
-	maven := platform(fs, `pom\.xml`, repoPath, "maven", schedule)
-	npm := platform(fs, `package\.json|package\-lock\.json`, repoPath, "npm", schedule)
-	nuget := platform(fs, `\.nuspec`, repoPath, "nuget", schedule)
-	pip := platform(fs, `requirements\.txt|requirement\.txt|Pipfile|Pipfile\.lock|setup\.py|requirements\.in|pyproject\.toml`, repoPath, "pip", schedule)
-	terraform := platform(fs, `(.*)\.tf`, repoPath, "terraform", schedule)
-	color.Style{color.FgLightBlue}.Print(" ■ ")
-	fmt.Println("Got platform ecosystems")
+	gomod := platform(afs, `go\.mod`, repoPath, "gomod", schedule)
+	gradle := platform(afs, `build\.gradle|build\.gradle\.kts`, repoPath, "gradle", schedule)
+	hexmix := platform(afs, `mix\.exs|mix\.lock`, repoPath, "mix", schedule)
+	maven := platform(afs, `pom\.xml`, repoPath, "maven", schedule)
+	npm := platform(afs, `package\.json|package\-lock\.json`, repoPath, "npm", schedule)
+	nuget := platform(afs, `\.nuspec`, repoPath, "nuget", schedule)
+	pip := platform(afs, `requirements\.txt|requirement\.txt|Pipfile|Pipfile\.lock|setup\.py|requirements\.in|pyproject\.toml`, repoPath, "pip", schedule)
+	terraform := platform(afs, `(.*)\.tf`, repoPath, "terraform", schedule)
+
+	log.Println("Got platform ecosystems")
+
 	updates := joinUpdates(bundler, cargo, composer, docker, elm, gitsubmodules, githubActual, gomod, gradle, hexmix, maven, npm, nuget, pip, terraform)
-	color.Style{color.FgLightBlue}.Print(" ■ ")
-	fmt.Println("Joined all updates")
-	config := Configuration{
+
+	log.Println("Joined all updates")
+
+	config = Configuration{
 		Version: 2,
 		Updates: updates,
 	}
-	color.Style{color.FgLightBlue}.Print(" ■ ")
-	fmt.Println("Configuration complete")
-	if verbose {
-		outputConfig(config)
-	}
-	writeConfig(fs, repoPath, config)
-	color.Style{color.FgGreen}.Print(" ■ ")
-	fmt.Println("Updated .github/dependabot.yml")
-	color.Style{color.FgGreen}.Print(" ■ ")
-	fmt.Println("Done!")
+
+	log.Println("Configuration complete")
+
+	err = writeConfig(afs, repoPath, config)
+	return
 }
 
-func platform(fs afero.Fs, regex string, repoPath string, ecosystem string, schedule Schedule) []Update {
-	dirs := directoryParser(fs, regex, repoPath)
-	uniqueDirs := removeDuplicates(dirs)
-	updates := updatesBuilder(uniqueDirs, ecosystem, schedule)
-	return updates
+func platform(afs *afero.Afero, regex string, repoPath string, ecosystem string, schedule Schedule) (updates []Update) {
+	uniqueDirs := removeDuplicates(directoryParser(afs, regex, repoPath))
+	updates = updatesBuilder(uniqueDirs, ecosystem, schedule)
+	return
 }
 
-func directoryParser(fs afero.Fs, regex string, repoPath string) []string {
-	fileDiscovery, err := FindFiles(fs, repoPath, regex)
+func directoryParser(afs *afero.Afero, regex string, repoPath string) []string {
+	fileDiscovery, err := file.FindByRegex(afs, repoPath, regex)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	var cleanDiscovery []string
 	for _, file := range fileDiscovery {
@@ -96,21 +93,18 @@ func directoryParser(fs afero.Fs, regex string, repoPath string) []string {
 	return directory
 }
 
-func removeDuplicates(stringSlice []string) []string {
+func removeDuplicates(stringSlice []string) (list []string) {
 	keys := make(map[string]bool)
-	list := []string{}
-
 	for _, entry := range stringSlice {
 		if _, value := keys[entry]; !value {
 			keys[entry] = true
 			list = append(list, entry)
 		}
 	}
-	return list
+	return
 }
 
-func updatesBuilder(directories []string, ecosystem string, schedule Schedule) []Update {
-	var updates []Update
+func updatesBuilder(directories []string, ecosystem string, schedule Schedule) (updates []Update) {
 	for _, dir := range directories {
 		update := Update{
 			PackageEcosystem: ecosystem,
@@ -119,43 +113,31 @@ func updatesBuilder(directories []string, ecosystem string, schedule Schedule) [
 		}
 		updates = append(updates, update)
 	}
-	return updates
+	return
 }
 
-func joinUpdates(updates ...[]Update) []Update {
-	var joinedUpdate []Update
+func joinUpdates(updates ...[]Update) (joinedUpdate []Update) {
 	for _, update := range updates {
 		joinedUpdate = append(joinedUpdate, update...)
 	}
 	if len(joinedUpdate) > 200 {
 		joinedUpdate = joinedUpdate[:200]
 	}
-	return joinedUpdate
+	return
 }
 
-func outputConfig(config Configuration) {
-	yamlOutput, err := yaml.Marshal(config)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println()
-	fmt.Println(string(yamlOutput))
-}
-
-func writeConfig(fs afero.Fs, repoPath string, config Configuration) {
+func writeConfig(afs *afero.Afero, repoPath string, config Configuration) (err error) {
 	githubPath := path.Join(repoPath, ".github", "dependabot.yml")
 	githubDir := path.Join(repoPath, ".github")
-	if _, err := fs.Stat(githubDir); os.IsNotExist(err) {
-		_ = fs.Mkdir(githubDir, 0755)
+	if _, err := afs.Stat(githubDir); os.IsNotExist(err) {
+		_ = afs.Mkdir(githubDir, 0755)
 	}
-	outFile, err := fs.OpenFile(githubPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	yaml, err := yaml.Marshal(config)
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
-	yamlEncoder := yaml.NewEncoder(outFile)
-	encodeErr := yamlEncoder.Encode(config)
-	if encodeErr != nil {
-		fmt.Println(encodeErr)
-	}
-	_ = yamlEncoder.Close()
+	log.Println(config)
+	log.Println("Writing configuration")
+	err = afs.WriteFile(githubPath, yaml, 0644)
+	return
 }
--- lib/generate_test.go
+++ lib/generate_test.go
@@ -1,27 +1,51 @@
 package lib
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestDirectoryParserGitHub(t *testing.T) {
-	fs := afero.NewMemMapFs()
-	_ = fs.Mkdir(".github", 0755)
-	_ = fs.Mkdir(".github/workflows", 0755)
-	_, _ = fs.Create(".github/workflows/ci.yml")
-	results := directoryParser(fs, `(.*)\.yml`, ".")
-	if results[0] != "/.github/workflows" {
-		t.Error()
+func TestGenerate(t *testing.T) {
+	afs := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+	afs.Create("go.mod")
+	afs.Create("requirements.txt")
+
+	schedule := Schedule{
+		Interval: "daily",
+		Time:     "",
+		TimeZone: "",
 	}
+	config, err := Generate(afs, ".", schedule)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, config.Version)
+	assert.Len(t, config.Updates, 2)
+	assert.NotNil(t, config)
+}
+
+func TestDirectoryParserGitHub(t *testing.T) {
+	afs := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+	_ = afs.Mkdir(".github", 0755)
+	_ = afs.Mkdir(".github/workflows", 0755)
+	_, _ = afs.Create(".github/workflows/ci.yml")
+	results := directoryParser(afs, `(.*)\.yml`, ".")
+
+	assert.Len(t, results, 1)
+	assert.Equal(t, "/.github/workflows", results[0])
 }
 
 func TestDirectoryParserGoMod(t *testing.T) {
-	fs := afero.NewMemMapFs()
-	_, _ = fs.Create("go.mod")
-	results := directoryParser(fs, `go\.mod`, ".")
+	afs := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+	_, _ = afs.Create("go.mod")
+	results := directoryParser(afs, `go\.mod`, ".")
+
+	assert.Len(t, results, 1)
+	assert.Equal(t, "/", results[0])
+
 	if results[0] != "/" {
 		t.Error()
 	}
@@ -31,7 +55,6 @@ func TestRemoveDuplicates(t *testing.T) {
 	sliceWithDupes := []string{"1", "1", "2", "2", "3", "3"}
 	correctSlice := []string{"1", "2", "3"}
 	uniqueSlice := removeDuplicates(sliceWithDupes)
-	if !reflect.DeepEqual(correctSlice, uniqueSlice) {
-		t.Error()
-	}
+
+	assert.ElementsMatch(t, correctSlice, uniqueSlice)
 }
